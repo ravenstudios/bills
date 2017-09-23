5b22b2de6dedacb3ed9c223eb6a1cb17d64b547d
ravenstudios <ravenstudiosrob@yahoo.com>
2017-09-22 23:28:47 -0500

    added databae connection

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var mongoose = require('mongoose'),
+    db = mongoose.createConnection('ravenstudios.ddns.net', 'bills');
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
